a36ed3a3a1b70d7ef23e46dca983e433e9e62850
SergeyKurnosov <kurnosovsergej123@mail.ru>
2025-07-23 20:27:20 +1000

    HW_21_07_2025

--- WindowsForms/MainForm.Designer.cs
+++ WindowsForms/MainForm.Designer.cs
@@ -31,11 +31,6 @@
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.labelCurrentTime = new System.Windows.Forms.Label();
-			this.timer = new System.Windows.Forms.Timer(this.components);
-			this.cbShowDate = new System.Windows.Forms.CheckBox();
-			this.cbShowWeekDay = new System.Windows.Forms.CheckBox();
-			this.btnHideControls = new System.Windows.Forms.Button();
-			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
 			this.cmMainMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
 			this.cmTopmost = new System.Windows.Forms.ToolStripMenuItem();
 			this.cmShowControls = new System.Windows.Forms.ToolStripMenuItem();
@@ -49,6 +44,25 @@
 			this.cmForeColor = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
 			this.cmClose = new System.Windows.Forms.ToolStripMenuItem();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.cbShowDate = new System.Windows.Forms.CheckBox();
+			this.cbShowWeekDay = new System.Windows.Forms.CheckBox();
+			this.btnHideControls = new System.Windows.Forms.Button();
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.rb2 = new System.Windows.Forms.RadioButton();
+			this.rb3 = new System.Windows.Forms.RadioButton();
+			this.rb1 = new System.Windows.Forms.RadioButton();
+			this.rb4 = new System.Windows.Forms.RadioButton();
+			this.rb5 = new System.Windows.Forms.RadioButton();
+			this.rbColor5 = new System.Windows.Forms.RadioButton();
+			this.rbColor4 = new System.Windows.Forms.RadioButton();
+			this.rbColor1 = new System.Windows.Forms.RadioButton();
+			this.rbColor3 = new System.Windows.Forms.RadioButton();
+			this.rbColor2 = new System.Windows.Forms.RadioButton();
+			this.rbImg4 = new System.Windows.Forms.RadioButton();
+			this.rbImg3 = new System.Windows.Forms.RadioButton();
+			this.rbImg2 = new System.Windows.Forms.RadioButton();
+			this.rbImg1 = new System.Windows.Forms.RadioButton();
 			this.cmMainMenu.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -58,58 +72,13 @@
 			this.labelCurrentTime.BackColor = System.Drawing.SystemColors.Control;
 			this.labelCurrentTime.ContextMenuStrip = this.cmMainMenu;
 			this.labelCurrentTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.labelCurrentTime.ForeColor = System.Drawing.SystemColors.ControlText;
 			this.labelCurrentTime.Location = new System.Drawing.Point(21, 9);
 			this.labelCurrentTime.Name = "labelCurrentTime";
 			this.labelCurrentTime.Size = new System.Drawing.Size(252, 51);
 			this.labelCurrentTime.TabIndex = 0;
 			this.labelCurrentTime.Text = "currentTime";
 			this.labelCurrentTime.DoubleClick += new System.EventHandler(this.labelCurrentTime_DoubleClick);
-			// 
-			// timer
-			// 
-			this.timer.Enabled = true;
-			this.timer.Tick += new System.EventHandler(this.timer_Tick);
-			// 
-			// cbShowDate
-			// 
-			this.cbShowDate.AutoSize = true;
-			this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.cbShowDate.Location = new System.Drawing.Point(21, 158);
-			this.cbShowDate.Name = "cbShowDate";
-			this.cbShowDate.Size = new System.Drawing.Size(193, 33);
-			this.cbShowDate.TabIndex = 1;
-			this.cbShowDate.Text = "Показать дату";
-			this.cbShowDate.UseVisualStyleBackColor = true;
-			// 
-			// cbShowWeekDay
-			// 
-			this.cbShowWeekDay.AutoSize = true;
-			this.cbShowWeekDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.cbShowWeekDay.Location = new System.Drawing.Point(21, 205);
-			this.cbShowWeekDay.Name = "cbShowWeekDay";
-			this.cbShowWeekDay.Size = new System.Drawing.Size(284, 33);
-			this.cbShowWeekDay.TabIndex = 2;
-			this.cbShowWeekDay.Text = "Показать день недели";
-			this.cbShowWeekDay.UseVisualStyleBackColor = true;
-			// 
-			// btnHideControls
-			// 
-			this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.btnHideControls.Location = new System.Drawing.Point(21, 265);
-			this.btnHideControls.Name = "btnHideControls";
-			this.btnHideControls.Size = new System.Drawing.Size(202, 75);
-			this.btnHideControls.TabIndex = 3;
-			this.btnHideControls.Text = "Скрыть элементы управления";
-			this.btnHideControls.UseVisualStyleBackColor = true;
-			this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
-			// 
-			// notifyIcon
-			// 
-			this.notifyIcon.ContextMenuStrip = this.cmMainMenu;
-			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
-			this.notifyIcon.Text = "notifyIcon";
-			this.notifyIcon.Visible = true;
-			this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
 			// 
 			// cmMainMenu
 			// 
@@ -144,6 +113,7 @@
 			this.cmShowControls.Size = new System.Drawing.Size(168, 22);
 			this.cmShowControls.Text = "show controls";
 			this.cmShowControls.CheckedChanged += new System.EventHandler(this.cmShowControls_CheckedChanged);
+			this.cmShowControls.Click += new System.EventHandler(this.cmShowControls_Click);
 			// 
 			// cmDebugConsole
 			// 
@@ -205,11 +175,248 @@
 			this.cmClose.Text = "close";
 			this.cmClose.Click += new System.EventHandler(this.cmClose_Click);
 			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// cbShowDate
+			// 
+			this.cbShowDate.AutoSize = true;
+			this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.cbShowDate.Location = new System.Drawing.Point(21, 158);
+			this.cbShowDate.Name = "cbShowDate";
+			this.cbShowDate.Size = new System.Drawing.Size(193, 33);
+			this.cbShowDate.TabIndex = 1;
+			this.cbShowDate.Text = "Показать дату";
+			this.cbShowDate.UseVisualStyleBackColor = true;
+			// 
+			// cbShowWeekDay
+			// 
+			this.cbShowWeekDay.AutoSize = true;
+			this.cbShowWeekDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.cbShowWeekDay.Location = new System.Drawing.Point(21, 205);
+			this.cbShowWeekDay.Name = "cbShowWeekDay";
+			this.cbShowWeekDay.Size = new System.Drawing.Size(284, 33);
+			this.cbShowWeekDay.TabIndex = 2;
+			this.cbShowWeekDay.Text = "Показать день недели";
+			this.cbShowWeekDay.UseVisualStyleBackColor = true;
+			// 
+			// btnHideControls
+			// 
+			this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btnHideControls.Location = new System.Drawing.Point(21, 265);
+			this.btnHideControls.Name = "btnHideControls";
+			this.btnHideControls.Size = new System.Drawing.Size(202, 84);
+			this.btnHideControls.TabIndex = 3;
+			this.btnHideControls.Text = "Скрыть элементы управления";
+			this.btnHideControls.UseVisualStyleBackColor = true;
+			this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.ContextMenuStrip = this.cmMainMenu;
+			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			this.notifyIcon.Text = "notifyIcon";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+			// 
+			// rb2
+			// 
+			this.rb2.AutoSize = true;
+			this.rb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rb2.Location = new System.Drawing.Point(389, 148);
+			this.rb2.Name = "rb2";
+			this.rb2.Size = new System.Drawing.Size(134, 20);
+			this.rb2.TabIndex = 5;
+			this.rb2.Text = "( 123 +-= abc абв ]";
+			this.rb2.UseVisualStyleBackColor = true;
+			this.rb2.CheckedChanged += new System.EventHandler(this.rb2_CheckedChanged);
+			// 
+			// rb3
+			// 
+			this.rb3.AutoSize = true;
+			this.rb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rb3.Location = new System.Drawing.Point(389, 171);
+			this.rb3.Name = "rb3";
+			this.rb3.Size = new System.Drawing.Size(134, 20);
+			this.rb3.TabIndex = 6;
+			this.rb3.Text = "( 123 +-= abc абв ]";
+			this.rb3.UseVisualStyleBackColor = true;
+			this.rb3.CheckedChanged += new System.EventHandler(this.rb3_CheckedChanged);
+			// 
+			// rb1
+			// 
+			this.rb1.AutoSize = true;
+			this.rb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rb1.Location = new System.Drawing.Point(389, 125);
+			this.rb1.Name = "rb1";
+			this.rb1.Size = new System.Drawing.Size(134, 20);
+			this.rb1.TabIndex = 7;
+			this.rb1.Text = "( 123 +-= abc абв ]";
+			this.rb1.UseVisualStyleBackColor = true;
+			this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+			// 
+			// rb4
+			// 
+			this.rb4.AutoSize = true;
+			this.rb4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rb4.Location = new System.Drawing.Point(389, 194);
+			this.rb4.Name = "rb4";
+			this.rb4.Size = new System.Drawing.Size(134, 20);
+			this.rb4.TabIndex = 8;
+			this.rb4.Text = "( 123 +-= abc абв ]";
+			this.rb4.UseVisualStyleBackColor = true;
+			this.rb4.CheckedChanged += new System.EventHandler(this.rb4_CheckedChanged);
+			// 
+			// rb5
+			// 
+			this.rb5.AutoSize = true;
+			this.rb5.Location = new System.Drawing.Point(389, 215);
+			this.rb5.Name = "rb5";
+			this.rb5.Size = new System.Drawing.Size(57, 17);
+			this.rb5.TabIndex = 9;
+			this.rb5.Text = "default";
+			this.rb5.UseVisualStyleBackColor = true;
+			this.rb5.CheckedChanged += new System.EventHandler(this.rb5_CheckedChanged);
+			// 
+			// rbColor5
+			// 
+			this.rbColor5.AutoSize = true;
+			this.rbColor5.ForeColor = System.Drawing.Color.Black;
+			this.rbColor5.Location = new System.Drawing.Point(389, 345);
+			this.rbColor5.Name = "rbColor5";
+			this.rbColor5.Size = new System.Drawing.Size(57, 17);
+			this.rbColor5.TabIndex = 14;
+			this.rbColor5.Text = "default";
+			this.rbColor5.UseVisualStyleBackColor = true;
+			this.rbColor5.CheckedChanged += new System.EventHandler(this.rbColor5_CheckedChanged);
+			// 
+			// rbColor4
+			// 
+			this.rbColor4.AutoSize = true;
+			this.rbColor4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rbColor4.ForeColor = System.Drawing.Color.HotPink;
+			this.rbColor4.Location = new System.Drawing.Point(389, 324);
+			this.rbColor4.Name = "rbColor4";
+			this.rbColor4.Size = new System.Drawing.Size(134, 20);
+			this.rbColor4.TabIndex = 13;
+			this.rbColor4.Text = "( 123 +-= abc абв ]";
+			this.rbColor4.UseVisualStyleBackColor = true;
+			this.rbColor4.CheckedChanged += new System.EventHandler(this.rbColor4_CheckedChanged);
+			// 
+			// rbColor1
+			// 
+			this.rbColor1.AutoSize = true;
+			this.rbColor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rbColor1.ForeColor = System.Drawing.Color.Red;
+			this.rbColor1.Location = new System.Drawing.Point(389, 255);
+			this.rbColor1.Name = "rbColor1";
+			this.rbColor1.Size = new System.Drawing.Size(134, 20);
+			this.rbColor1.TabIndex = 12;
+			this.rbColor1.Text = "( 123 +-= abc абв ]";
+			this.rbColor1.UseVisualStyleBackColor = true;
+			this.rbColor1.CheckedChanged += new System.EventHandler(this.rbColor1_CheckedChanged);
+			// 
+			// rbColor3
+			// 
+			this.rbColor3.AutoSize = true;
+			this.rbColor3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rbColor3.ForeColor = System.Drawing.Color.SeaGreen;
+			this.rbColor3.Location = new System.Drawing.Point(389, 301);
+			this.rbColor3.Name = "rbColor3";
+			this.rbColor3.Size = new System.Drawing.Size(134, 20);
+			this.rbColor3.TabIndex = 11;
+			this.rbColor3.Text = "( 123 +-= abc абв ]";
+			this.rbColor3.UseVisualStyleBackColor = true;
+			this.rbColor3.CheckedChanged += new System.EventHandler(this.rbColor3_CheckedChanged);
+			// 
+			// rbColor2
+			// 
+			this.rbColor2.AutoSize = true;
+			this.rbColor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rbColor2.ForeColor = System.Drawing.Color.Orange;
+			this.rbColor2.Location = new System.Drawing.Point(389, 278);
+			this.rbColor2.Name = "rbColor2";
+			this.rbColor2.Size = new System.Drawing.Size(134, 20);
+			this.rbColor2.TabIndex = 10;
+			this.rbColor2.Text = "( 123 +-= abc абв ]";
+			this.rbColor2.UseVisualStyleBackColor = true;
+			this.rbColor2.CheckedChanged += new System.EventHandler(this.rbColor2_CheckedChanged);
+			// 
+			// rbImg4
+			// 
+			this.rbImg4.AutoSize = true;
+			this.rbImg4.ForeColor = System.Drawing.Color.Black;
+			this.rbImg4.Location = new System.Drawing.Point(389, 79);
+			this.rbImg4.Name = "rbImg4";
+			this.rbImg4.Size = new System.Drawing.Size(57, 17);
+			this.rbImg4.TabIndex = 18;
+			this.rbImg4.Text = "default";
+			this.rbImg4.UseVisualStyleBackColor = true;
+			this.rbImg4.CheckedChanged += new System.EventHandler(this.rbImg4_CheckedChanged);
+			// 
+			// rbImg3
+			// 
+			this.rbImg3.AutoSize = true;
+			this.rbImg3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbImg3.BackgroundImage")));
+			this.rbImg3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rbImg3.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.rbImg3.Location = new System.Drawing.Point(389, 58);
+			this.rbImg3.Name = "rbImg3";
+			this.rbImg3.Size = new System.Drawing.Size(51, 20);
+			this.rbImg3.TabIndex = 17;
+			this.rbImg3.Text = "lake";
+			this.rbImg3.UseVisualStyleBackColor = true;
+			this.rbImg3.CheckedChanged += new System.EventHandler(this.rbImg3_CheckedChanged);
+			// 
+			// rbImg2
+			// 
+			this.rbImg2.AutoSize = true;
+			this.rbImg2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbImg2.BackgroundImage")));
+			this.rbImg2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rbImg2.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.rbImg2.Location = new System.Drawing.Point(389, 35);
+			this.rbImg2.Name = "rbImg2";
+			this.rbImg2.Size = new System.Drawing.Size(63, 20);
+			this.rbImg2.TabIndex = 16;
+			this.rbImg2.Text = "ocean";
+			this.rbImg2.UseVisualStyleBackColor = true;
+			this.rbImg2.CheckedChanged += new System.EventHandler(this.rbImg2_CheckedChanged);
+			// 
+			// rbImg1
+			// 
+			this.rbImg1.AutoSize = true;
+			this.rbImg1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rbImg1.BackgroundImage")));
+			this.rbImg1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.rbImg1.ForeColor = System.Drawing.SystemColors.Info;
+			this.rbImg1.Location = new System.Drawing.Point(389, 12);
+			this.rbImg1.Name = "rbImg1";
+			this.rbImg1.Size = new System.Drawing.Size(54, 20);
+			this.rbImg1.TabIndex = 19;
+			this.rbImg1.Text = "stars";
+			this.rbImg1.UseVisualStyleBackColor = true;
+			this.rbImg1.CheckedChanged += new System.EventHandler(this.rbImg1_CheckedChanged);
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(379, 370);
+			this.ClientSize = new System.Drawing.Size(616, 370);
+			this.Controls.Add(this.rbImg1);
+			this.Controls.Add(this.rbImg4);
+			this.Controls.Add(this.rbImg3);
+			this.Controls.Add(this.rbImg2);
+			this.Controls.Add(this.rbColor5);
+			this.Controls.Add(this.rbColor4);
+			this.Controls.Add(this.rbColor1);
+			this.Controls.Add(this.rbColor3);
+			this.Controls.Add(this.rbColor2);
+			this.Controls.Add(this.rb5);
+			this.Controls.Add(this.rb4);
+			this.Controls.Add(this.rb1);
+			this.Controls.Add(this.rb3);
+			this.Controls.Add(this.rb2);
 			this.Controls.Add(this.btnHideControls);
 			this.Controls.Add(this.cbShowWeekDay);
 			this.Controls.Add(this.cbShowDate);
@@ -218,6 +425,7 @@
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
 			this.Text = "Clock";
+			this.Load += new System.EventHandler(this.MainForm_Load);
 			this.cmMainMenu.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -245,6 +453,20 @@
 		private System.Windows.Forms.ToolStripMenuItem cmForeColor;
 		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
 		private System.Windows.Forms.ToolStripMenuItem cmClose;
+		private System.Windows.Forms.RadioButton rb2;
+		private System.Windows.Forms.RadioButton rb3;
+		private System.Windows.Forms.RadioButton rb1;
+		private System.Windows.Forms.RadioButton rb4;
+		private System.Windows.Forms.RadioButton rb5;
+		private System.Windows.Forms.RadioButton rbColor5;
+		private System.Windows.Forms.RadioButton rbColor4;
+		private System.Windows.Forms.RadioButton rbColor1;
+		private System.Windows.Forms.RadioButton rbColor3;
+		private System.Windows.Forms.RadioButton rbColor2;
+		private System.Windows.Forms.RadioButton rbImg4;
+		private System.Windows.Forms.RadioButton rbImg3;
+		private System.Windows.Forms.RadioButton rbImg2;
+		private System.Windows.Forms.RadioButton rbImg1;
 	}
 }
 
--- WindowsForms/MainForm.cs
+++ WindowsForms/MainForm.cs
@@ -7,39 +7,149 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Text;
 using System.Runtime.InteropServices;
 
 namespace WindowsForms
 {
 	public partial class MainForm : Form
 	{
+		PrivateFontCollection fontCollection = new PrivateFontCollection();
+		string[] fileNames = {
+		"D:/ProjectHW/WindowsForms/WindowsForms/FONTS/lastfunk-street.colr_.ttf" ,
+		"D:/ProjectHW/WindowsForms/WindowsForms/FONTS/YujiHentaiganaAkebono-Regular.ttf",
+		"D:/ProjectHW/WindowsForms/WindowsForms/FONTS/Quantico-Bold.ttf",
+		"D:/ProjectHW/WindowsForms/WindowsForms/FONTS/PinyonScript-Regular.ttf"};
+		string[] fileimages ={
+		"D:/ProjectHW/WindowsForms/WindowsForms/BACKIMAGE/fonstola.ru_138046.jpg",
+		"D:/ProjectHW/WindowsForms/WindowsForms/BACKIMAGE/spongeBob.jpeg",
+		"D:/ProjectHW/WindowsForms/WindowsForms/BACKIMAGE/Без названия.jpeg",
+		"none"};
+		int indexForChoiceImage = 3;
+
+
 		public MainForm()
 		{
 			InitializeComponent();
-			ShowControls(cmShowControls.Checked);
+			loadFontsFromFilesToCollection(fontCollection, fileNames);
+			ShowControls(cmShowControls.Checked = true);
+
 		}
 
 		void ShowControls(bool visible)
 		{
+			RadioButton[] rbFonts = new RadioButton[] { rb1, rb2, rb3, rb4, rb5 };
+			RadioButton[] rbColors = new RadioButton[] { rbColor1, rbColor2, rbColor3, rbColor4, rbColor5 };
+			RadioButton[] rbImages = new RadioButton[] { rbImg1, rbImg2, rbImg3, rbImg4 };
+
 			cbShowDate.Visible = visible;
 			cbShowWeekDay.Visible = visible;
 			btnHideControls.Visible = visible;
+			cmShowControls.Checked = visible;
+			for (int i = 0; i < rbFonts.Length; i++)
+			{
+				rbFonts[i].Visible = visible;
+				rbFonts[i].BackColor = visible ? Color.Transparent : Color.White;
+			}
+			for (int i = 0; i < rbColors.Length; i++)
+			{
+				rbColors[i].Visible = visible;
+				rbColors[i].BackColor = visible ? Color.Transparent : Color.White;
+			}
+
+
+			for (int i = 0; i < rbImages.Length; i++)
+			{
+				rbImages[i].Visible = visible;
+
+			}
+			rbImages[rbImages.Length - 1].BackColor = visible ? Color.Transparent : Color.White;
+
 			this.ShowInTaskbar = visible;
 			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
 			this.FormBorderStyle = visible ? FormBorderStyle.FixedDialog : FormBorderStyle.None;
-			this.labelCurrentTime.BackColor = visible ? this.BackColor : Color.DeepSkyBlue;
+			//	this.labelCurrentTime.BackColor = visible ? this.BackColor : Color.DeepSkyBlue;
+		    this.labelCurrentTime.BackColor = visible ? this.BackColor : Color.Transparent;
+			this.labelCurrentTime.Image = visible ? null : this.BackgroundImage;
+			this.cbShowDate.Image = visible ? null : this.BackgroundImage;
+			this.cbShowWeekDay.Image = visible ? null : this.BackgroundImage;
+			this.btnHideControls.Image = visible ? null : this.BackgroundImage;
+			this.rbImg4.Image = visible ? null : this.BackgroundImage;
+			if (visible)
+				setImageBackground(fileimages[indexForChoiceImage]);
+			else
+				this.BackgroundImage = null;
+
 		}
 
 		void ShowConsole(bool visible)
 		{
-			//	bool console = visible ? AllocConsole() : FreeConsole();
-			//if(console)Console.WriteLine(console);
 			if (visible)
 				AllocConsole();
 			else
 				FreeConsole();
 		}
 
+		void loadFontsFromFilesToCollection(PrivateFontCollection fontCollection, string[] fileNames)
+		{
+
+			for (int i = 0; i < fileNames.Length; i++)
+			{
+				fontCollection.AddFontFile(fileNames[i]);
+			}
+
+			rb1.Font = new Font(fontCollection.Families[0], this.rb1.Font.Size);
+			rb2.Font = new Font(fontCollection.Families[1], this.rb2.Font.Size);
+			rb3.Font = new Font(fontCollection.Families[2], this.rb3.Font.Size);
+			rb4.Font = new Font(fontCollection.Families[3], this.rb4.Font.Size);
+		}
+
+		void setFont(FontFamily newFont)
+		{
+			this.labelCurrentTime.Font = new Font(newFont, this.labelCurrentTime.Font.Size);
+			this.cbShowDate.Font = new Font(newFont, this.cbShowDate.Font.Size);
+			this.cbShowWeekDay.Font = new Font(newFont, this.cbShowWeekDay.Font.Size);
+			this.btnHideControls.Font = new Font(newFont, this.btnHideControls.Font.Size);
+		}
+
+		void setColorText(Color newColor)
+		{
+			this.labelCurrentTime.ForeColor = newColor;
+			this.cbShowDate.ForeColor = newColor;
+			this.cbShowWeekDay.ForeColor = newColor;
+			this.btnHideControls.ForeColor = newColor;
+
+			this.rb1.ForeColor = this.rb2.ForeColor = this.rb3.ForeColor = this.rb4.ForeColor = newColor;
+		}
+
+		void setImageBackground(string pathImage)
+		{
+			if (pathImage == "none")
+			{
+				this.BackgroundImage = this.btnHideControls.BackgroundImage = null;
+			}
+			else
+			{
+				this.BackgroundImage = Image.FromFile(pathImage);
+				this.BackgroundImageLayout = ImageLayout.Stretch;
+
+				this.labelCurrentTime.BackColor = Color.Transparent;
+				this.cbShowDate.BackColor = Color.Transparent;
+				this.cbShowWeekDay.BackColor = Color.Transparent;
+				this.btnHideControls.BackgroundImage = Image.FromFile(pathImage);
+
+				for (int i = 0; i < pathImage.Length; i++)
+				{
+
+				}
+			}
+
+
+
+		}
+
+
+		///////////////////////////////////////////////////////////////////////////////////////////
 
 		private void timer_Tick(object sender, EventArgs e)
 		{
@@ -58,7 +168,8 @@ namespace WindowsForms
 
 		private void btnHideControls_Click(object sender, EventArgs e)
 		{
-			ShowControls(cmShowControls.Checked = false);
+			cmShowControls.Checked = false;
+			ShowControls(cmShowControls.Checked);
 		}
 
 		private void labelCurrentTime_DoubleClick(object sender, EventArgs e)
@@ -87,7 +198,7 @@ namespace WindowsForms
 		[DllImport("kernel32.dll")]
 		public static extern bool FreeConsole();
 
-		/// /////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////
 
 
 		private void cmDebugConsole_CheckedChanged(object sender, EventArgs e)
@@ -100,6 +211,115 @@ namespace WindowsForms
 			if (this.TopMost) return;
 			this.TopMost = true;
 			this.TopMost = false;
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			int screenWidth = Screen.PrimaryScreen.WorkingArea.Width,
+				screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+
+			this.Location = cmShowControls.Checked ?
+				new System.Drawing.Point(screenWidth - this.Size.Width, 0) :
+				new System.Drawing.Point(screenWidth - this.labelCurrentTime.Size.Width + this.labelCurrentTime.Location.X, 0 - this.labelCurrentTime.Location.Y);
+
+
+
+			//		this.BackgroundImage = Image.FromFile("D:/ProjectHW/WindowsForms/WindowsForms/BACKIMAGE/fonstola.ru_138046.jpg");
+			//		this.BackgroundImageLayout = ImageLayout.Stretch;
+
+			//		labelCurrentTime.BackColor = Color.Transparent;
+
+		}
+
+		private void rb1_CheckedChanged(object sender, EventArgs e)
+		{
+			FontFamily newFont = fontCollection.Families[0];
+			setFont(newFont);
+		}
+
+		private void rb2_CheckedChanged(object sender, EventArgs e)
+		{
+			FontFamily newFont = fontCollection.Families[1];
+			setFont(newFont);
+		}
+
+		private void rb3_CheckedChanged(object sender, EventArgs e)
+		{
+			FontFamily newFont = fontCollection.Families[2];
+			setFont(newFont);
+		}
+
+		private void rb4_CheckedChanged(object sender, EventArgs e)
+		{
+			FontFamily newFont = fontCollection.Families[3];
+			setFont(newFont);
+		}
+
+		private void rb5_CheckedChanged(object sender, EventArgs e)
+		{
+			this.labelCurrentTime.Font = new Font("Microsoft Sans Serif", this.labelCurrentTime.Font.Size);
+			this.cbShowDate.Font = new Font("Microsoft Sans Serif", this.cbShowDate.Font.Size);
+			this.cbShowWeekDay.Font = new Font("Microsoft Sans Serif", this.cbShowWeekDay.Font.Size);
+			this.btnHideControls.Font = new Font("Microsoft Sans Serif", this.btnHideControls.Font.Size);
+		}
+
+		private void rbColor1_CheckedChanged(object sender, EventArgs e)
+		{
+			Color newColor = Color.Red;
+			setColorText(newColor);
+		}
+
+		private void rbColor2_CheckedChanged(object sender, EventArgs e)
+		{
+			Color newColor = Color.Orange;
+			setColorText(newColor);
+		}
+
+		private void rbColor3_CheckedChanged(object sender, EventArgs e)
+		{
+			Color newColor = Color.SeaGreen;
+			setColorText(newColor);
+		}
+
+		private void rbColor4_CheckedChanged(object sender, EventArgs e)
+		{
+			Color newColor = Color.HotPink;
+			setColorText(newColor);
+		}
+
+		private void rbColor5_CheckedChanged(object sender, EventArgs e)
+		{
+			Color newColor = Color.Black;
+			setColorText(newColor);
+		}
+
+		private void rbImg1_CheckedChanged(object sender, EventArgs e)
+		{
+			indexForChoiceImage = 0;
+			setImageBackground(fileimages[indexForChoiceImage]);
+		}
+
+		private void rbImg2_CheckedChanged(object sender, EventArgs e)
+		{
+			indexForChoiceImage = 1;
+			setImageBackground(fileimages[indexForChoiceImage]);
+		}
+
+		private void rbImg3_CheckedChanged(object sender, EventArgs e)
+		{
+			indexForChoiceImage = 2;
+			setImageBackground(fileimages[indexForChoiceImage]);
+		}
+
+		private void rbImg4_CheckedChanged(object sender, EventArgs e)
+		{
+			indexForChoiceImage = 3;
+			setImageBackground(fileimages[indexForChoiceImage]);
+		}
+
+		private void cmShowControls_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
